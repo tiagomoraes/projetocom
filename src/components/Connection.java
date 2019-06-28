114b8f5d3b3aa60f84c2079a7f057094c8fc793b
Matheus Belfort de Moura Torres <mbmt@WINDOWS.CIN.UFPE.BR>
2019-06-28 11:43:09 -0300

    atualizado connection, Main, add messageQueue

--- src/components/Connection.java
+++ src/components/Connection.java
@@ -5,64 +5,128 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-
+import java.util.concurrent.Semaphore;
 
 public class Connection extends Thread {
+	private Socket socket;
 	private ObjectInputStream MyInput;
 	private ObjectOutputStream MyOutput;
 	private String ip;
+	private ArrayList<Message> messageVector;
 	private ArrayList<Connection> connectionVector;
+	private Semaphore semaphore;
 
-	public Connection(Socket socket, ArrayList<Connection> connection) {
+	public Connection(Socket socket, ArrayList<Message> messageVector, ArrayList<Connection> connectionVector,
+			Semaphore semaphore) {
 		super();
 		try {
-			this.MyInput = new ObjectInputStream(socket.getInputStream());
+			this.socket = socket;
 			this.MyOutput = new ObjectOutputStream(socket.getOutputStream());
+			this.MyInput = new ObjectInputStream(socket.getInputStream());
+			this.ip = socket.getInetAddress().toString().replaceAll("/", "");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		this.semaphore = semaphore;
+		this.messageVector = messageVector;
+		this.connectionVector = connectionVector;
 	}
-	
+
 	public String getIp() {
 		return this.ip;
 	}
-	
+
 	public void sendMessage(Message msg) throws IOException {
-		this.MyOutput.writeObject(msg);
+		if (isConnected()) {
+			this.MyOutput.writeObject(msg);
+			this.MyOutput.flush();
+		} else {
+			System.out.println("add msg no messageVector");
+			this.messageVector.add(msg);
+		}
+	}
+
+	public void reconnect(Socket socket) throws IOException {
+		this.socket = socket;
+		this.MyOutput = new ObjectOutputStream(socket.getOutputStream());
+		this.MyInput = new ObjectInputStream(socket.getInputStream());
+	}
+
+	public boolean isConnected() {
+		return !socket.isClosed();
 	}
 
 	public void run() {
-		while(true) {
+		while (true) {
 			try {
-				Message msg = (Message)this.MyInput.readObject();
-				this.ip = msg.getSender();
-				if(msg.getStatus() == 0) {
-					msg.setStatus(1);
-					this.MyOutput.writeObject(msg);//confirmou recebimento pelo servidor para sender
-					//enviar pro reciver
-					for(int i = 0; i < this.connectionVector.size();i++) {
-						if(connectionVector.get(i).getIp().equals(msg.getReceiver())) {
-							connectionVector.get(i).sendMessage(msg);
+				if (this.isConnected()) {
+					Message msg = (Message) this.MyInput.readObject();
+					if (msg.getStatus() == 0) { // pending
+						msg.setStatus(1); // sent
+						this.MyOutput.writeObject(msg);// confirmou recebimento pelo servidor para sender
+						this.MyOutput.flush();
+						// enviar pro reciver
+						try {
+							semaphore.acquire();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
 						}
-					}
-				}else if(msg.getStatus() == 2) {
-					this.MyOutput.writeObject(msg);//ok
-				}else if(msg.getStatus() == 3) {
-					this.MyOutput.writeObject(msg);//ok
-				}else if(msg.getStatus() == 4) {
-					for(int i = 0; i < this.connectionVector.size();i++) {
-						if(connectionVector.get(i).getIp().equals(msg.getReceiver())) {
-							connectionVector.get(i).sendMessage(msg);
+						System.out.println("status é 0");
+						for (int i = 0; i < this.connectionVector.size(); i++) {
+							if (connectionVector.get(i).getIp().equals(msg.getReceiver())) {
+								System.out.println("encontrado ip certo");
+								connectionVector.get(i).sendMessage(msg);
+								this.MyOutput.flush();
+							}
 						}
+						semaphore.release();
+					} else if (msg.getStatus() == 2) {
+						try {
+							semaphore.acquire();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						for (int i = 0; i < this.connectionVector.size(); i++) {
+							if (connectionVector.get(i).getIp().equals(msg.getSender())) {
+								connectionVector.get(i).sendMessage(msg);
+								this.MyOutput.flush();
+							}
+						}
+						semaphore.release();
+					} else if (msg.getStatus() == 3) {
+						this.MyOutput.writeObject(msg);// OK
+						this.MyOutput.flush();
+					} else if (msg.getStatus() == 4) {
+						try {
+							semaphore.acquire();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						for (int i = 0; i < this.connectionVector.size(); i++) {
+							if (connectionVector.get(i).getIp().equals(msg.getReceiver())) {
+								connectionVector.get(i).sendMessage(msg);
+								this.MyOutput.flush();
+							}
+						}
+						semaphore.release();
+					} else {
+						// erro?
 					}
-				}else {
-					//erro?
+				} else {
+					return;
 				}
+
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
-				e.printStackTrace();
+				try {
+					this.MyInput.close();
+					this.MyOutput.close();
+					this.socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				break;
 			}
 		}
 	}
--- src/components/Main.java
+++ src/components/Main.java
@@ -3,18 +3,62 @@ package components;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		int port = 3001;
+		@SuppressWarnings("resource")
 		ServerSocket tmpsocket = new ServerSocket(port);
-		ArrayList<Connection> connectionVector = null;
+		ArrayList <Message> messageVector = new ArrayList<Message>();
+		ArrayList<Connection> connectionVector = new ArrayList<Connection>();
+		Semaphore semaphore = new Semaphore(1);
+		//MessageQueue messageQueue = new MessageQueue(messageVector,connectionVector,semaphore);
+		//messageQueue.start();
+		boolean hasAlreadyConnected;
 
 		while (true) {
-			int index = 0;
-			Connection connection = new Connection(tmpsocket.accept(), connectionVector);
-			connectionVector.add(index, connection);
+			hasAlreadyConnected = false;
+			Connection connection = new Connection(tmpsocket.accept(), messageVector, connectionVector, semaphore);
+			try {
+				semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			for (int x = 0; x < connectionVector.size(); x++) {
+				if (connection.getIp().equals(connectionVector.get(x).getIp())) {
+					hasAlreadyConnected = true;
+					connectionVector.get(x).reconnect(tmpsocket.accept());
+					for (int i = 0; i < messageVector.size(); i++) {
+						for (int j = 0; j < connectionVector.size(); j++) {
+							try {
+								semaphore.acquire();
+							} catch (InterruptedException e1) {
+								e1.printStackTrace();
+							}
+							if (connectionVector.get(j).isConnected()
+									&& messageVector.get(i).getReceiver().equals(connectionVector.get(j).getIp())) {
+
+								try {
+									connectionVector.get(j).sendMessage(messageVector.get(i));
+									messageVector.remove(messageVector.get(i));
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+							}
+						}
+						semaphore.release();
+					}
+				}
+			}
+
+			if (!hasAlreadyConnected) {
+				connectionVector.add(connection);
+			}
+			semaphore.release();
 			connection.start();
+			System.out.println(connectionVector.size());
+
 		}
 	}
 }
