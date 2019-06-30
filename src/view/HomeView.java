aeade17818f13f136b0e323c680a8c2d83f1d2bb
Vinicius Dantas Januario <vdj@ug3c04.windows.cin.ufpe.br>
2019-06-30 19:27:11 -0300

    view por enquanto

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -1,6 +1,5 @@
 package view;
 
-
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -67,22 +66,24 @@ public class HomeView {
 	private int lastRead;
 	private int toSend;
 	private connect tryTo;
-	
+	private boolean moveOff;
+
 	/**
 	 * Launch the application.
 	 */
-	//arg0 = myip
-	//arg1 = myname;
-	//arg2 = serverip;
-	//arg3 = auserip;
-	//arg4 = ausername;
-	//arg5 = myport;
+	// arg0 = myip
+	// arg1 = myname;
+	// arg2 = serverip;
+	// arg3 = auserip;
+	// arg4 = ausername;
+	// arg5 = myport;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HomeView window = new HomeView(args[0], args[1], args[2] , args[3], args[4],Integer.parseInt(args[5]));
+					HomeView window = new HomeView(args[0], args[1], args[2], args[3], args[4],
+							Integer.parseInt(args[5]));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -106,11 +107,12 @@ public class HomeView {
 		this.sem = new Semaphore(1);
 		this.messageArr = new Vector<Message>();
 		this.toSend = 0;
+		this.moveOff = false;
 
 		// Initialize jPanel
 		initialize();
 		this.frame.setVisible(true);
-		
+
 		// connect to server
 		this.tryTo = new connect();
 		tryTo.start();
@@ -125,7 +127,10 @@ public class HomeView {
 		frame.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (lastRead != messageArr.size()) {
+				if (tryTo.isAlive()) {
+					moveOff = true;
+				}
+				if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
 					sendMessage("-1", "-1", "-1");
 				}
 			}
@@ -135,10 +140,13 @@ public class HomeView {
 		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 
 		JPanel panel = new JPanel();
-		panel.addKeyListener(new KeyAdapter() {
+		panel.addMouseListener(new MouseAdapter() {
 			@Override
-			public void keyPressed(KeyEvent e) {
-				if (lastRead != messageArr.size()) {
+			public void mouseClicked(MouseEvent e) {
+				if (tryTo.isAlive()) {
+					moveOff = true;
+				}
+				if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
 					sendMessage("-1", "-1", "-1");
 				}
 			}
@@ -151,7 +159,10 @@ public class HomeView {
 		scrollPane.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (lastRead != messageArr.size()) {
+				if (tryTo.isAlive()) {
+					moveOff = true;
+				}
+				if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
 					sendMessage("-1", "-1", "-1");
 				}
 			}
@@ -163,7 +174,10 @@ public class HomeView {
 		boxMessages.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (lastRead != messageArr.size()) {
+				if (tryTo.isAlive()) {
+					moveOff = true;
+				}
+				if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
 					sendMessage("-1", "-1", "-1");
 				}
 			}
@@ -177,100 +191,138 @@ public class HomeView {
 		textInput.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
+				if (tryTo.isAlive()) {
+					moveOff = true;
+				}
 				int key = e.getKeyCode();
+				System.out.println("teste key: " + lastRead);
 				if (key == KeyEvent.VK_ENTER) {
 					if (!textInput.getText().equals("")) {
-						sendMessage(myip, auserip,textInput.getText());
+						if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
+							sendMessage("-1", "-1", "-1");
+						}
+						sendMessage(myip, auserip, textInput.getText());
+					} else {
+						if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
+							sendMessage("-1", "-1", "-1");
+						}
 					}
 				} else {
-					if (lastRead != messageArr.size()) {
+					// System.out.println("teste key");
+					if (messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
 						sendMessage("-1", "-1", "-1");
 					}
 				}
 			}
 		});
-		
-		// poder ser tocar no scroll ou em alguma parte do app, digitar alguma coisa la na box etc. 
-		// ler msg le le tudo na tela q o outro enviou (só manda uma msg contendo o indice no vetor da ultima msg enviada por sender)
+
+		// poder ser tocar no scroll ou em alguma parte do app, digitar alguma coisa la
+		// na box etc.
+		// ler msg le le tudo na tela q o outro enviou (só manda uma msg contendo o
+		// indice no vetor da ultima msg enviada por sender)
 		// ter um boolean ou alguma condição pra nao mandar msg redundante
 		// action read message
 		// private void readMessage() {
 		// //send message with 3
 		// }
-		
+
 		// pode ser um botao no lado de cada msg ou um double click
-		// se for msg do outro usuario, só apaga localmente, se for sua manda uma msg com 4 e o indice no seu vetor
+		// se for msg do outro usuario, só apaga localmente, se for sua manda uma msg
+		// com 4 e o indice no seu vetor
 		// action delete message
 		// private void deleteMessage() {
 		// //send message with 4
 		// }
 	}
 
-	private void sendMessage(String sender, String receiver,String message) {
-		Message msg = new Message(sender, receiver,message);
+	private void sendMessage(String sender, String receiver, String message) {
+		Message msg = new Message(sender, receiver, message);
 		// Code to send message TCP
 		try {
 			sem.acquire();
-			if (!msg.getSender().equals("-1")) {
+			if (msg.getSender().equals("-1")) {
+				msg.setStatus(3);
+			} else if (msg.getSender().equals("-2")) {
+				msg.setStatus(4);
+			} else if (!msg.getSender().equals("-1")) {
 				msg.setPs(messageArr.size());
 				msg.setPr(-1);
 				messageArr.add(msg);
-				this.lastRead = messageArr.size();
+				//System.out.println("GOT THERE");
+				// this.lastRead = messageArr.size();
 			}
+			if (tryTo.isAlive()) System.out.println("omegalul");
 			if (!tryTo.isAlive()) {
+				//System.out.println("GOT THERE");
 				if (msg.getStatus() == 3) {
-					msg.setPs(messageArr.get(messageArr.size()-1).getPs());
-					msg.setPr(messageArr.get(messageArr.size()-1).getPr());
+
+					msg.setSender(this.auserip);
+					msg.setReceiver(this.myip);
+					msg.setPs(messageArr.get(messageArr.size() - 1).getPs());
+					msg.setPr(messageArr.get(messageArr.size() - 1).getPr());
 					this.lastRead = messageArr.size();
-				} else if (msg.getStatus() == 4) { 
-					//msg.setPs(messageArr.get(messageArr.size()-1).getPs()); ver como vai fazer
-					//msg.setPr(messageArr.get(messageArr.size()-1).getPr()); ver como vai fazer 
+					sendTCPMessage(msg);
+				} else if (msg.getStatus() == 4) {
+					// msg.setPs(messageArr.get(messageArr.size()-1).getPs()); ver como vai fazer
+					// msg.setPr(messageArr.get(messageArr.size()-1).getPr()); ver como vai fazer
+					updateMessages(msg);
 				} else {
 					for (int i = this.toSend; i < messageArr.size(); i++) {
+						//System.out.println("GOT THERE");
 						if (messageArr.get(i).getSender().equals(this.myip)) {
 							sendTCPMessage(messageArr.get(i));
 						}
+
 					}
+
+					// this.lastRead = messageArr.size();
 				}
-		
-			}			
-			updateMessages(msg);
+			}
+			if (msg.getStatus() == 0)updateMessages(msg);
 			sem.release();
 		} catch (Exception e) {
 			sem.release();
-			if (!tryTo.isAlive()) {
-				tryTo.start();
-			}
+			//if (!tryTo.isAlive()) {
+			tryTo = new connect();
+			tryTo.start();
+			//}
 		}
 		this.textInput.setText("");
 	}
 
 	private void sendTCPMessage(Message m) throws Exception {
 		try {
+			System.out.println(toSend);
 			myOutput.writeObject(m);
 			myOutput.flush();
-			this.toSend++;
+			if (m.getStatus() == 0) this.toSend++;
+			System.out.println(toSend);
 		} catch (Exception e) {
 			throw e;
 		}
 	}
 
 	private void updateMessages(Message i) {
+		//System.out.println("bug");
 		boxMessages.removeAll();
 		if (this.myip.equals(i.getSender())) {
 			messageArr.get(i.getPs()).setStatus(i.getStatus());
 			if (i.getStatus() == 3) {
-				for (int j = this.lastRead; j < messageArr.size(); j++) {
+				for (int j = i.getPr(); j >= 0 ; j--) {
 					this.messageArr.get(j).setStatus(3);
 				}
 			} else if (i.getStatus() == 2) {
 				messageArr.get(i.getPs()).setPr(i.getPr());
+			} else if (i.getStatus() == 1) {
+				//
 			}
+
 		} else if (this.myip.equals(i.getReceiver())) {
 			messageArr.get(i.getPr()).setStatus(i.getStatus());
 			if (i.getStatus() == 4) messageArr.get(i.getPr()).setMessage("----DELETED----");
-		}	
+		}
 		for (Message m : messageArr) {
+			// System.out.println(m.getMessage());
 			// Display messages
 			JPanel gridMessage = new JPanel();
 			boxMessages.add(gridMessage);
@@ -278,11 +330,11 @@ public class HomeView {
 			JLabel txtMessage = new JLabel();
 			if (m.getSender().equals(this.myip)) {
 				txtMessage = new JLabel(this.myname + ": " + m.getMessage());
-			} else if (m.getReceiver().equals(this.auserip)) {
+			} else if (m.getSender().equals(this.auserip)) {
 				txtMessage = new JLabel(this.ausername + ": " + m.getMessage());
-			} 
+			}
 			gridMessage.add(txtMessage);
-			
+
 			if (m.getSender().equals(myip)) {
 				JLabel statusMessage = new JLabel(String.valueOf(m.getStatus()));
 				gridMessage.add(statusMessage);
@@ -292,12 +344,13 @@ public class HomeView {
 		boxMessages.revalidate();
 		boxMessages.repaint();
 	}
-	
+
 	class connect extends Thread {
-		public connect () {
-			super ();
+		public connect() {
+			super();
 		}
-		public void run () {
+
+		public void run() {
 			while (true) {
 				try {
 					sem.acquire();
@@ -307,7 +360,7 @@ public class HomeView {
 					sem.release();
 					new listen().start();
 					break;
-				} catch (Exception e){
+				} catch (Exception e) {
 					try {
 						sem.release();
 						Thread.sleep(5000);
@@ -321,27 +374,38 @@ public class HomeView {
 	}
 
 	class listen extends Thread {
-		public listen () {
+		public listen() {
 			super();
 		}
+
 		public void run() {
 			while (true) {
 				try {
 					while (tryTo.isAlive()) {
 						Thread.sleep(100);
 					}
+					sem.acquire();
+					if (moveOff && messageArr.size() != 0 && !tryTo.isAlive() && !messageArr.lastElement().getSender().equals(myip) && lastRead != messageArr.size()) {
+						sendMessage("-1", "-1", "-1");
+					}
+					moveOff = false;
+					System.out.println(toSend);
 					if (messageArr.size() != toSend) {
 						for (int i = toSend; i < messageArr.size(); i++) {
+							System.out.println("kaka");
 							if (messageArr.get(i).getSender().equals(myip)) {
 								sendTCPMessage(messageArr.get(i));
 							}
-							toSend = i+1;
+							toSend = i + 1;
 						}
 					}
+
+					sem.release();
+
 					Message m = (Message) myInput.readObject();
 
 					sem.acquire();
-					if (m.getStatus() == 1 && !m.getReceiver().contains(myip)) {
+					if (m.getStatus() == 1 && m.getSender().equals(myip)) {
 						updateMessages(m);
 					} else if (m.getStatus() == 1 && m.getReceiver().contains(myip)) {
 						Message aux = new Message(m.getSender(), m.getReceiver(), "");
@@ -351,12 +415,15 @@ public class HomeView {
 						messageArr.add(m);
 						updateMessages(m);
 						aux.setStatus(2);
-						System.out.println("sent2");
+						// lastRead = messageArr.size();
+						// System.out.println("sent2");
 						sendTCPMessage(aux);
-						
+
 					} else if (m.getStatus() == 2) {
+						//System.out.println(m.getMessage() + ' ' + m.getSender() + ' ' + m.getReceiver());
 						updateMessages(m);
 					} else if (m.getStatus() == 3) {
+						//lastRead = messageArr.size();
 						updateMessages(m);
 					} else if (m.getStatus() == 4) {
 						updateMessages(m);
@@ -364,10 +431,10 @@ public class HomeView {
 					sem.release();
 				} catch (Exception e) {
 					sem.release();
-					if (!tryTo.isAlive()) {
-						tryTo = new connect();
-						tryTo.start();
-					}
+					//if (!tryTo.isAlive()) {
+					tryTo = new connect();
+					tryTo.start();
+					//}
 					break;
 				}
 			}
